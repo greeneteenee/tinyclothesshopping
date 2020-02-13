9b6140367aeb4cc3b4488772464a9de1a79b5dc5
Tina Greene <greene9408@student.cptc.edu>
2020-02-13 10:52:24 -0800

    Implemented basic registration

---
+++ TinyClothes/Data/AccountDb.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+using System.Linq;
+using TinyClothes.Data;
+using TinyClothes.Models;
+
+namespace TinyClothes
+{
+    public static class AccountDb
+    {
+        public static async Task<bool> IsUsernameTaken(string username, StoreContext context)
+        {
+            bool isTaken = await (from acc in context.Accounts
+                                where username == acc.Username
+                                select acc).AnyAsync();
+            return isTaken;
+        }
+
+        public static async Task<Account> Register(StoreContext context, Account acc)
+        {
+            await context.Accounts.AddAsync(acc);
+            await context.SaveChangesAsync();
+            return acc;
+        }
+    }
+}
